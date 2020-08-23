a172ab0570b4e90981a2174c7e9ef62b71c30248
RaulPeSilva <adraulpesilva@gmail.com>
2020-08-23 03:49:06 -0300

    add adsense

--- src/pages/bem/index.js
+++ src/pages/bem/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import TextArea from '../../components/shared/TextArea';
 import * as Styled from './styles';
 // teste__teste1-teste2
+import AdSense from 'react-adsense';
 
 const Bem = () => {
   const ref = useRef(null);
@@ -14,29 +15,29 @@ const Bem = () => {
       const [first, children] = item.split('__');
       const sub = children?.split('--');
       console.log('aaa', first, children, sub);
-      sub?.length > 0 ?
-      sub.reduce((oldClass, classe, index) => {
-        if (index) {
-          composed[first][oldClass] = { ...composed[first][oldClass], [`--${classe}`]: {} };
-            return oldClass;
-          }
-          if (index === 0) {
-            if (composed?.[first]?.[`__${classe}`]) {
-              return `__${classe}`;
-            } else {
-              composed[first] = composed[first]
-              ? { ...composed[first], [`__${classe}`]: {} }
-                : { [`__${classe}`]: {} };
+      sub?.length > 0
+        ? sub.reduce((oldClass, classe, index) => {
+            if (index) {
+              composed[first][oldClass] = { ...composed[first][oldClass], [`--${classe}`]: {} };
+              return oldClass;
+            }
+            if (index === 0) {
+              if (composed?.[first]?.[`__${classe}`]) {
+                return `__${classe}`;
+              } else {
+                composed[first] = composed[first]
+                  ? { ...composed[first], [`__${classe}`]: {} }
+                  : { [`__${classe}`]: {} };
               }
-            return `__${classe}`;
-          }
-          return oldClass;
-        }, '')
-        : composed[first] = {};
-      });
+              return `__${classe}`;
+            }
+            return oldClass;
+          }, '')
+        : (composed[first] = {});
+    });
     return composed;
   };
-  
+
   const extractClass = (element) => {
     const elementList = [...element.querySelectorAll('*')];
     const elementClassList = elementList.reduce((acc, item) => {
@@ -58,15 +59,24 @@ const Bem = () => {
   };
 
   return (
-    <Styled.Container>
-      <Styled.FieldWrapper>
-        <TextArea placeholder="Input" onChange={handleChange}></TextArea>
-      </Styled.FieldWrapper>
-      <Styled.FieldWrapper>
-        <TextArea placeholder="Output" value={output}></TextArea>
-      </Styled.FieldWrapper>
-      <div ref={ref} style={{ display: 'none' }}></div>
-    </Styled.Container>
+    <>
+      <Styled.Container>
+        <Styled.FieldWrapper>
+          <TextArea placeholder="Input" onChange={handleChange}></TextArea>
+        </Styled.FieldWrapper>
+        <Styled.FieldWrapper>
+          <TextArea placeholder="Output" value={output}></TextArea>
+        </Styled.FieldWrapper>
+        <div ref={ref} style={{ display: 'none' }}></div>
+      </Styled.Container>
+      <AdSense.Google
+        client="ca-pub-7292810486004926"
+        slot="7806394673"
+        style={{ display: 'block' }}
+        layout="in-article"
+        format="fluid"
+      />
+    </>
   );
 };
 const generateBemSass = (classes) => {
